b7acd2b116d440629b816a3cc220199ced6aaab4
Dongwoo Kim <124665715+elecbrandy@users.noreply.github.com>
2026-01-19 19:04:59 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string  input;
+    int     n;
+
+    cin >> input;
+    cout << input.length();
+    return 0;
+}
