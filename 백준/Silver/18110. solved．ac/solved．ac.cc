de7203025ebe0eb5601ed9e3f4374a13fa734528
Dongwoo Kim <124665715+elecbrandy@users.noreply.github.com>
2026-01-30 18:33:54 +0900

    [Silver IV] Title: solved.ac, Time: 24 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.cc
+++ 백준/Silver/18110. solved．ac/solved．ac.cc
@@ -1,25 +1,44 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <algorithm>
+#include <list>
+#include <cmath>
 
 using namespace std;
 
-int main()
-{
-    int n;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n = 0;
+    int result = 0;
+    
     cin >> n;
-    if (n == 0) cout << 0;
-    else {
-        vector<int> rate(n, 0);
-        for (int i = 0; i < n; i++) {
-            cin >> rate[i];
-        }
-        sort(rate.begin(), rate.end());
-        int t = round((double)n / 100 * 15), divnum = n - (t * 2), sum = 0;
-        for (int i = t; i < n - t; i++) {
-            sum += rate[i];
-        }
-        cout << round((double)sum / divnum);
+    if (n == 0) {
+        cout << '0';
+        return 0;
     }
+    
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    // 제외인원 = 전체의 30%
+    double ex_num = ((double)n /100 * 15);
+    int tmp = round(ex_num);
+
+
+    int sum = 0;
+    for (int i = tmp; i < v.size() - tmp; i++) {
+        sum += v[i];
+    }
+    double av = (double)sum / (v.size() - (tmp * 2));
+    result = round(av);
+
+    cout << result;
+    return 0;
 }
