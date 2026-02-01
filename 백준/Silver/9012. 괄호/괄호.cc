d8963dc3cf6a2d9747cdfd8be7297d86ef1ec195
Dongwoo Kim <124665715+elecbrandy@users.noreply.github.com>
2026-02-02 00:49:37 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+bool ifBalance(string &input) {
+    stack<char> s;
+    string tmp = "()[]";
+
+    for (int i = 0; i < input.size(); i++) {
+
+        if (tmp.find(input[i]) == string::npos) {
+            continue;
+        }
+
+        // 여는 괄호: 스택에 넣기
+        if (input[i] == '(') {
+            s.push(input[i]);
+            continue;
+        }
+
+        // 닫는 괄호: 스택에서 꺼내기
+        if (input[i] == ')') {
+            if (s.size() == 0)
+                return false;
+            if (s.top() == '(') {
+                s.pop();
+            } else {
+                return false;
+            }
+            
+        }
+    }
+    return (s.size() == 0) ? true : false;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n = 0;
+    cin >> n;
+    cin.ignore(); 
+    for (int i = 0; i < n; i++) {
+        string input;
+        getline(cin, input);
+        cout << (ifBalance(input) ? "YES\n" : "NO\n");
+    }
+    return 0;
+}
